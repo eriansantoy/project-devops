6f6f357e2f9867eaaaee1aef077bdfaa14366797
eriansantoy <leonardo88erian@gmail.com>
2026-04-07 02:05:29 +0000

    feat: detener instancia EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -10,3 +10,7 @@ def listar():
 def iniciar(instance_id):
     ec2.start_instances(InstanceIds=[instance_id])
     print("Instancia iniciada")
+
+def detener(instance_id):
+    ec2.stop_instances(InstanceIds=[instance_id])
+    print("Instancia detenida")
