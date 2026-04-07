0c2d547868d9b4f0699f5f8c4a26a62a9103dc65
eriansantoy <leonardo88erian@gmail.com>
2026-04-07 02:00:17 +0000

    feat: iniciar instancia EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -6,3 +6,7 @@ ec2 = boto3.client('ec2')
 def listar():
     response = ec2.describe_instances()
     print(response)
+
+def iniciar(instance_id):
+    ec2.start_instances(InstanceIds=[instance_id])
+    print("Instancia iniciada")
